3fca1e439ce6ad983ad801be454e3dad42d19a85
webnai <email>
2023-06-14 19:23:49 +0000

    seconds in month calculator

---
+++ seconds.js
@@ -0,0 +1,10 @@
+/*How many seconds are in 30 days
+ * days = 30
+ * day = 24 hours
+ * hour = 60 minutes */
+var day = 24;
+var hour = 60;
+var minute = 60;
+
+var seconds = ((((minute*hour)*day)*30))
+console.log(seconds);
