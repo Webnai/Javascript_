8eff3dc3092d2c811ef1f46954913e6b04e620d0
webnai <email>
2023-06-23 20:19:00 +0000

    discount calculator

---
+++ discount.js
@@ -0,0 +1,6 @@
+let purchase = 2000;
+let discount = (purchase/100) * 20;
+let good = ' great you got a discount of';
+let bad = 'too bad you didn\'t get a discount';
+
+if (purchase>=2000){console.log(good + " " + discount + " " +  'so your total is =' + " " + (purchase - discount))};
